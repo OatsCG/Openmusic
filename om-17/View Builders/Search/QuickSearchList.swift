5b83a37d7eca8a2e28907ea41b599696c7f63505
OatsCG <charlesg2003@hotmail.com>
2024-04-17 17:43:03 -0400

    Fixed preview compiling

--- om-17/View Builders/Search/QuickSearchList.swift
+++ om-17/View Builders/Search/QuickSearchList.swift
@@ -68,7 +68,7 @@ struct QuickSearchList: View {
     let playlist = StoredPlaylist(Title: "Test!")
     container.mainContext.insert(playlist)
     
-    return SearchPage(searchNSPath: .constant(NavigationPath()), selectionBinding: .constant(1))
+    return SearchPage(searchNSPath: .constant(NavigationPath()))
         .modelContainer(container)
         .environment(PlayerManager())
         .environment(PlaylistImporter())
